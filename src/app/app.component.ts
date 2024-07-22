6a0563db0022e673ed0fc87070d306da41c316c3
David Silvente <dsilvente2@gmail.com>
2024-07-23 00:53:25 +0200

    Implementacion traductor pagina web

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,28 @@
 import { Component, HostListener } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
-import { trigger, state, style, animate, transition } from '@angular/animations';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  animations: [
-    trigger('fadeIn', [
-      state('void', style({
-        opacity: 0
-      })),
-      transition(':enter', [
-        animate('1s ease-in')
-      ])
-    ])
-  ]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Portfolio';
   currentSection: string = 'me';
   isHomePage: boolean = true;
+  currentLang: string = 'es';
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private translate: TranslateService) {
     // Suscribirse a los eventos de navegación para determinar la ruta actual
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
         this.isHomePage = (event.url === '/' || event.url === '/home');
       }
     });
+
+    // Establecer el idioma por defecto
+    this.translate.setDefaultLang(this.currentLang);
   }
 
   ngOnInit(): void {}
@@ -40,10 +34,15 @@ export class AppComponent {
     }
   }
 
+  switchLanguage() {
+    this.currentLang = this.currentLang === 'es' ? 'en' : 'es';
+    this.translate.use(this.currentLang);
+  }
+
   @HostListener('window:scroll', [])
   onWindowScroll() {
     const navbar = document.getElementById('navbar');
-    const menu = document.getElementById('menu')
+    const menu = document.getElementById('menu');
     const sections = document.querySelectorAll('section');
     let currentSectionId = '';
 
@@ -54,7 +53,7 @@ export class AppComponent {
       }
     });
 
-    this.currentSection = currentSectionId // Default to 'me' if no section is found
+    this.currentSection = currentSectionId; // Default to 'me' if no section is found
 
     if (window.pageYOffset > 50) {
       menu?.classList.add('bg-black', 'shadow-lg');
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,38 +9,40 @@ import { AboutComponent } from './about/about.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ContactComponent } from './contact/contact.component';
 import { FormsModule, ReactiveFormsModule   } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { ProjectCardComponent } from './projects/project-card/project-card.component';
 import { CorreoService } from './correo.service';
 import { MessageSuccessFailComponent } from './message-success-fail/message-success-fail.component';
 import { ProjectPageComponent } from './projects/project-page/project-page.component';
 import { ThemeToggleComponent } from './theme-toggle/theme-toggle.component';
 import { ThemeService } from './theme.service';
-import { ɵBrowserAnimationBuilder } from '@angular/animations';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    AboutComponent,
-    ProjectsComponent,
-    ContactComponent,
-    ProjectCardComponent,
-    MessageSuccessFailComponent,
-    ProjectPageComponent,
-    ThemeToggleComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule,
-    ReactiveFormsModule,
-    
-  ],
-  providers: [CorreoService,
-    ThemeService
-  ],
-  bootstrap: [AppComponent]
-})
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
+@NgModule({ declarations: [
+        AppComponent,
+        HomeComponent,
+        AboutComponent,
+        ProjectsComponent,
+        ContactComponent,
+        ProjectCardComponent,
+        MessageSuccessFailComponent,
+        ProjectPageComponent,
+        ThemeToggleComponent,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        ReactiveFormsModule, TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [HttpClient]
+          }
+        })], providers: [CorreoService,
+        ThemeService, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
